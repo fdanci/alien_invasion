8a69e005562ccc1eccf6cd5cde59c3fbcfc015a4
fdanci <florin.danci96@gmail.com>
2020-09-06 16:23:39 +0300

    Add space ship to project

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize game's settings."""
+        # Screen settings.
+        self.bg_color = (255, 255, 255)
+        self.screen_width = 1200
+        self.screen_height = 800
